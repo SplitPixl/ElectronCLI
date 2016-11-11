6b5793e318f5c6352e256cf692718156466b9af2
SplitPixl <splitpixl@gmail.com>
2016-11-11 15:56:47 -0800

    Sanatized inputs, added printToCLI()

--- src/commands.js
+++ src/commands.js
@@ -1,48 +1,54 @@
 const appData = require('../package.json');
+var doc
 
 module.exports = {
-	run(input) {
+	run(input, document) {
+    doc = document
     var cinput = input.toLowerCase();
-	let args = cinput.split(' ');
+	  let args = cinput.split(' ');
     switch (args[0]) {
-		
+
 		// Small Commands
 		case "ping":
-			return "Pong!";
+			printToCLI("Pong!");
 		break;
-		
+
 		case "8ball":
 			let ballresp = require('./ballresp.json');
 			let determine = ((Math.random() * (ballresp.length - 1) + 1).toFixed(0) - 1);
-			return "<br>Oh, interesting question. The 8ball's response is <u>" + ballresp[determine] + "</u>.";
+			printToCLI("<br>Oh, interesting question. The 8ball's response is <u>" + ballresp[determine] + "</u>.");
 		break;
-		
+
 		case "clear":
-			return "Matt was told to make this command, he failed miserably.";
+			doc.getElementById("buffer") = ""
 		break;
-		
+
 		case "about":
-			return "ElectronCLI is a Command-Line interface built from Electron.<br>ElectronCLI was (and currently is being) developed by " + appData.author + ".";
+			printToCLI("ElectronCLI is a Command-Line interface built from Electron.<br>ElectronCLI was (and currently is being) developed by " + appData.author + ".");
 		break;
-		
-		case "eval":
-			let argstring = args.slice(1).join(" ");
-			return eval(argstring);
-		break;
-		
+
+
+
 		// Large Commands
-		
+
 		case "help":
-			return "You are using ElectronCLI v" + appData.version + ", created by " + appData.author + ".<br><br><strong><u>Commands:</u></strong><br><br>" + 
-			"8ball - Let the 8 ball decide!<br>" +
-			"about - More information on ElectronCLI.<br>" + 
-			"ping - Pong!";
+      let commands = [
+			"You are using ElectronCLI v" + appData.version + ", created by " + appData.author + ".<br><br><strong><u>Commands:</u></strong><br><br>",
+			"8ball - Let the 8 ball decide!",
+			"about - More information on ElectronCLI.",
+			"ping - Pong!"
+      ]
+      printToCLI(commands.join("<br>"));
 		break;
-		
+
 		// End of commands
-		
+
 		default:
-			return "Invalid syntax! Type \"help\" for infomation on how to use ElectronCLI.";
+			printToCLI("Invalid syntax! Type \"help\" for infomation on how to use ElectronCLI.");
 	}
-	}
+  function printToCLI(output) {
+    var buffer = doc.getElementById("buffer").innerHTML
+    doc.getElementById("buffer").innerHTML = buffer + prompt + input + "<br>" + output + "<br><br>"
+  }
+  }
 };
