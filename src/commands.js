42b07e6d25b5a82f7322409b8ae1c093c5450537
MattSilverblood <heysilverblood@outlook.com>
2016-11-11 20:22:38 +0000

    Improved command interpreter method

--- src/commands.js
+++ src/commands.js
@@ -1,11 +1,13 @@
 module.exports = {
 	run(input) {
     var cinput = input.toLowerCase();
-    if(cinput === "ping") {
-      return "Pong!";
-    }
-    else {
-      return "Command not found."
-    }
+    switch (cinput) {
+		case "ping":
+			return "Pong!";
+		break;
+		case "about":
+			return "ElectronCLI is a Command-Line interface built from Electron.<br>ElectronCLI was (and currently is being) developed by MattSilverblood and SplitPixl.";
+		break;
+	}
 	}
 };
