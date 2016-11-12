ecb141b882b254746568f0c6fd5b78ccf57f5178
SplitPixl <splitpixl@gmail.com>
2016-11-12 09:15:06 -0800

    I made a thing!

---
+++ src/cmds/what.js
@@ -0,0 +1,21 @@
+var e = module.exports = {};
+const BrowserWindow = require('electron').remote.BrowserWindow
+
+e.init = () => {
+
+};
+e.requireCtx = require;
+
+e.isCommand = true;
+e.hidden = true;
+e.usage = 'what';
+e.info = 'What is this?';
+e.longinfo = '<p>???</p>';
+
+e.execute = (input, args, document) => {
+    let win = new BrowserWindow({ width: 400, height: 320 })
+    win.on('close', function () { win = null })
+    win.loadURL("https://splitpixl.xyz/splart")
+    win.show()
+    printToCLI("")
+};
