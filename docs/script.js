a2c50f565b9045940af8a932d83cc9aa6309fbc9
MattSilverblood <heysilverblood@outlook.com>
2016-11-15 18:04:15 +0000

    Started off website

---
+++ docs/script.js
@@ -0,0 +1,7 @@
+function goToGithub() {
+    document.location.href = "https://github.com/SplitPixl/ElectronCLI";
+}
+
+function goToDL() {
+    document.location.href = "https://github.com/SplitPixl/ElectronCLI/releases";
+}
